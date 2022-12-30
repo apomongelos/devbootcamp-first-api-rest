ad9f7571404d01b66a692548bd03e8aa20909067
Fernando Mongelos <fernandomongelos911@gmail.com>
2022-12-30 19:33:59 -0300

    crear modelos utilizando sequelize

---
+++ models/tasks.js
@@ -0,0 +1,9 @@
+const { DataTypes } = require('sequelize');
+const db = require('./index');
+
+const tasksModel = db.define('Tasks', {
+  name: DataTypes.STRING,
+  description: DataTypes.STRING,
+});
+
+module.exports = tasksModel;
